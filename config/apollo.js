0164e35c1f508a0e419fdfdcbe4cc483617494b4
Jorge Luis Vera <veravelis3@gmail.com>
2021-12-15 14:51:28 -0300

    primer commit

---
+++ config/apollo.js
@@ -0,0 +1,29 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import fetch from 'node-fetch'
+
+const client = new ApolloClient({
+    connectToDevTools: true,
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'http://localhost:4000/',
+        fetch
+    })
+});
+
+export default client;
+
+
+
+//   import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+//   import fetch from 'node-fetch'
+
+//   const client = new ApolloClient({
+//       cache: new InMemoryCache(),
+//       link: new HttpLink({
+//           uri: 'http://localhost:4000',
+//           fetch
+//       })
+//   });
+
+//   export default client;
+
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const Index = () => {
   return ( 
     <Layout>
-      <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+      <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
     </Layout>
 
    );
